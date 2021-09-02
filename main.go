11fcc9baf6eb5aab686da9ca204813f4b862d1a2
jooonyong <leejy0718@naver.com>
2021-09-02 19:50:14 +0900

    modify blockchain and open server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello form home!")
+}
 func main() {
-	fmt.Println("It works")
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on https://localhost %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
