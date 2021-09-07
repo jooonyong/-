6964143a41919c5094c16cac8bc8166fac467dda
jooonyong <leejy0718@naver.com>
2021-09-07 13:47:49 +0900

    server side

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "github.com/jooonyong/junyongcoin/explorer"
 
-const port string = ":4000"
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello form home!")
-}
 func main() {
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on https://localhost %s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
